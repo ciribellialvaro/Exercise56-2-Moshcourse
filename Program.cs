c8afece2eb8acfaedcd06abb52041d3795af74ba
Álvaro Borges <gestor.alvaro@gmail.com>
2022-04-09 23:43:44 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise56_2_Moshcourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //2 - Write a program and ask the user to enter their name. Use an array to reverse
+            //    the name and then store the result in a new string.Display the reversed name on the console.
+
+            string name;
+            Console.Write("Type your name ");
+            name = Console.ReadLine();
+            var r = name.ToArray();
+            Array.Reverse(r);
+            Console.WriteLine(r);
+
+
+
+        }
+    }
+}
